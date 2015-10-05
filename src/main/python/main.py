2252582dde82d60306f6db0767e2197ba6df69eb
jaoki <you@example.com>
2015-10-05 03:06:47 -0700

    added replacement

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -6,6 +6,17 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/..
 TARGET_DIR = PROJECT_ROOT + "/target"
 RESOURCES_DIR = PROJECT_ROOT + "/src/main/resources"
 
+def replace_to_jp(root):
+    ens_parents = root.findall(".//*[@ok_lang='en']/..")
+    for ens_parent in ens_parents:
+        en = ens_parent.find("*[@ok_lang='en']")
+        jp = en.find("*[@ok_lang='jp']")
+        if jp is not None:
+            ens_parent.remove(en)
+            ens_parent.append(jp)
+#         parent = en.getparent()
+
+
 def main():
     print "project root: " + PROJECT_ROOT
 
@@ -13,11 +24,12 @@ def main():
         os.mkdir(TARGET_DIR)
 
     root = ET.parse(RESOURCES_DIR + "/html/index.html").getroot()
-    ens_parents = root.findall(".//*[@ok_lang='en']/..")
-    for ens_parent in ens_parents:
-        target = ens_parent.find(".//*[@ok_lang='en']")
-#         parent = en.getparent()
-        ens_parent.remove(target)
+    replace_to_jp(root)
+#     ens_parents = root.findall(".//*[@ok_lang='en']/..")
+#     for ens_parent in ens_parents:
+#         target = ens_parent.find("*[@ok_lang='en']")
+# #         parent = en.getparent()
+#         ens_parent.remove(target)
 
 #     root.remove(a)
     print minidom.parseString(ET.tostring(root)).toprettyxml()
